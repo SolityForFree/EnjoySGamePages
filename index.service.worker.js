35ae493b7b1a3b6ee5096a2095f8902ac2c55c0f
SolityForFree <strax950@gmail.com>
2024-01-15 16:56:43 +0100

    Correct custom html page.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1705333134|104951239098";
+const CACHE_VERSION = "1705334070|105887558987";
 const CACHE_PREFIX = "EnjoySGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
