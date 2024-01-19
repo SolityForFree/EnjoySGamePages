b0d45903a86a77a4bccb8b02c66d93e2ece8bf6a
SolityForFree <strax950@gmail.com>
2024-01-19 13:18:09 +0100

    Added new popup info box.

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1705622098|7602251314";
+const CACHE_VERSION = "1705664946|5690238033";
 const CACHE_PREFIX = "EnjoySGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
